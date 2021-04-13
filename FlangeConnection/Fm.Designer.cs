ffb29562d633f386545ea60890ec74f971aad0a1
Кристина Базаева <bazaeva.kristeny@gmail.com>
2021-04-13 19:10:28 +0300

    интерфейс

--- FlangeConnection/Fm.Designer.cs
+++ FlangeConnection/Fm.Designer.cs
@@ -29,34 +29,172 @@ namespace FlangeConnection
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.picBox1 = new System.Windows.Forms.PictureBox();
+            this.picBox2 = new System.Windows.Forms.PictureBox();
+            this.picBox3 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.picBoxPaint = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.panel = new System.Windows.Forms.FlowLayoutPanel();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxPaint)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // picBox1
             // 
-            this.button1.Location = new System.Drawing.Point(587, 185);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(94, 29);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.picBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.picBox1.Image = global::FlangeConnection.Properties.Resources.pic1;
+            this.picBox1.Location = new System.Drawing.Point(12, 12);
+            this.picBox1.Name = "picBox1";
+            this.picBox1.Size = new System.Drawing.Size(400, 250);
+            this.picBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picBox1.TabIndex = 0;
+            this.picBox1.TabStop = false;
+            // 
+            // picBox2
+            // 
+            this.picBox2.BackColor = System.Drawing.SystemColors.Window;
+            this.picBox2.Image = global::FlangeConnection.Properties.Resources.pic2;
+            this.picBox2.Location = new System.Drawing.Point(12, 628);
+            this.picBox2.Name = "picBox2";
+            this.picBox2.Size = new System.Drawing.Size(400, 250);
+            this.picBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picBox2.TabIndex = 1;
+            this.picBox2.TabStop = false;
+            // 
+            // picBox3
+            // 
+            this.picBox3.BackColor = System.Drawing.SystemColors.Window;
+            this.picBox3.Image = global::FlangeConnection.Properties.Resources.pic3;
+            this.picBox3.Location = new System.Drawing.Point(12, 314);
+            this.picBox3.Name = "picBox3";
+            this.picBox3.Size = new System.Drawing.Size(400, 250);
+            this.picBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picBox3.TabIndex = 2;
+            this.picBox3.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 278);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(260, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Фланец с шейкой, приварной встык";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 900);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(212, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = " Плоский приварной фланец";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 588);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(152, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = " Свободные фланцы";
+            // 
+            // picBoxPaint
+            // 
+            this.picBoxPaint.BackColor = System.Drawing.SystemColors.Window;
+            this.picBoxPaint.Location = new System.Drawing.Point(790, 35);
+            this.picBoxPaint.Name = "picBoxPaint";
+            this.picBoxPaint.Size = new System.Drawing.Size(465, 395);
+            this.picBoxPaint.TabIndex = 6;
+            this.picBoxPaint.TabStop = false;
+            this.picBoxPaint.Paint += new System.Windows.Forms.PaintEventHandler(this.picBox_Paint);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(790, 12);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(115, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "График чего-то";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(428, 47);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(103, 20);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Выбор среды";
+            // 
+            // panel
+            // 
+            this.panel.AutoScroll = true;
+            this.panel.BackColor = System.Drawing.SystemColors.Window;
+            this.panel.Location = new System.Drawing.Point(428, 79);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(345, 183);
+            this.panel.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(428, 12);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(82, 20);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Парметры";
             // 
             // Fm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(914, 600);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(1293, 938);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.panel);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.picBoxPaint);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.picBox3);
+            this.Controls.Add(this.picBox2);
+            this.Controls.Add(this.picBox1);
             this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MinimumSize = new System.Drawing.Size(1311, 985);
             this.Name = "Fm";
-            this.Text = "FlangeConnection";
+            this.Text = "kmkkfmkmkfmkvm";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.picBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxPaint)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox picBox1;
+        private System.Windows.Forms.PictureBox picBox2;
+        private System.Windows.Forms.PictureBox picBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox picBoxPaint;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.FlowLayoutPanel panel;
+        private System.Windows.Forms.Label label6;
     }
 }
 
--- FlangeConnection/Fm.cs
+++ FlangeConnection/Fm.cs
@@ -12,10 +12,225 @@ namespace FlangeConnection
 {
     public partial class Fm : Form
     {
+        float k = 1; //коэффициент
+        float n = 2; //степень графика
+        float f = 0; //x+
+        float c = 0; //y+
+
         public Fm()
         {
             InitializeComponent();
+            picBox1.MouseLeave += PicBox_MouseLeave;
+            picBox2.MouseLeave += PicBox_MouseLeave;
+            picBox3.MouseLeave += PicBox_MouseLeave;
+
+            picBox1.MouseEnter += PicBox_MouseEnter;
+            picBox2.MouseEnter += PicBox_MouseEnter;
+            picBox3.MouseEnter += PicBox_MouseEnter;
+
+            createCheckBox();
+            createSize();
+            this.Resize += (s, e) => createSize();
+
+
         }
 
+        private void createSize()
+        {
+            //локация выбора конструкции
+            int picWidth = this.ClientSize.Width / 45 * 10;
+            int picHeight = this.ClientSize.Height / 40 * 10;
+            picBox1.Size = new Size(picWidth, picHeight);
+            picBox2.Size = new Size(picWidth, picHeight);
+            picBox3.Size = new Size(picWidth, picHeight);
+
+            picBox1.Location = new Point(15,15);
+            picBox2.Location = new Point(15,15 * 3 + picHeight);
+            picBox3.Location = new Point(15, 15 * 5 + picHeight*2);
+
+            label1.Location = new Point(15, 15 + picHeight);
+            label2.Location = new Point(15, 15 * 3 + picHeight*2);
+            label3.Location = new Point(15, 15 * 5 + picHeight * 3);
+            
+
+            // локация параметров
+            int panelX = this.ClientSize.Width / 35 * 10;
+            int panelY = this.ClientSize.Height / 80 * 10-30;
+            int panelY5 = this.ClientSize.Height / 80 * 10-60;
+            int panelY6 = this.ClientSize.Height / 80 * 10-90;
+            panel.Location = new Point(panelX, panelY);
+            label5.Location = new Point(panelX, panelY5);
+            label6.Location = new Point(panelX, panelY6);
+
+            //локация графика
+            int grafX = this.ClientSize.Width / 5 * 3;
+            int grafY = this.ClientSize.Height / 80 * 10-30;
+            int grafLable = this.ClientSize.Height / 80 * 10-90;
+            picBoxPaint.Location = new Point(grafX, grafY);
+            label4.Location = new Point(grafX, grafLable);
+            int grafWidth = this.ClientSize.Width / 3 ;
+            int grafHeight = this.ClientSize.Height / 3 ;
+            picBoxPaint.Size = new Size(grafWidth, grafHeight);
+            picBoxPaint.Invalidate();
+        }
+
+        private void createCheckBox()
+        {
+            CheckBox checkBox1 = new CheckBox();
+            checkBox1.Text = "Вода";
+            checkBox1.Size = new Size(230, 30);
+            panel.Controls.Add(checkBox1);
+
+            CheckBox checkBox2 = new CheckBox();
+            checkBox2.Text = "Воздух";
+            checkBox2.Size = new Size(230, 30);
+            panel.Controls.Add(checkBox2);
+
+            CheckBox checkBox3 = new CheckBox();
+            checkBox3.Text = "Нейтральные расворы солей";
+            checkBox3.Size = new Size(230,30);
+            panel.Controls.Add(checkBox3);
+
+            CheckBox checkBox4 = new CheckBox();
+            checkBox4.Text = "Нейтральные газы";
+            checkBox4.Size = new Size(230, 30);
+            panel.Controls.Add(checkBox4);
+
+            CheckBox checkBox19 = new CheckBox();
+            checkBox19.Text = "Нейтральные пары";
+            checkBox19.Size = new Size(230, 30);
+            panel.Controls.Add(checkBox19);
+
+            CheckBox checkBox5 = new CheckBox();
+            checkBox5.Text = "HSO концентрация до 56%";
+            checkBox5.Size = new Size(230, 30);
+            panel.Controls.Add(checkBox5);
+
+            CheckBox checkBox6 = new CheckBox();
+            checkBox6.Text = "Тяжелые нефтепродукты";
+            checkBox6.Size = new Size(230, 30);
+            panel.Controls.Add(checkBox6);
+
+            CheckBox checkBox7 = new CheckBox();
+            checkBox7.Text = "Керосин";
+            checkBox7.Size = new Size(230, 30);
+            panel.Controls.Add(checkBox7);
+
+            CheckBox checkBox8 = new CheckBox();
+            checkBox8.Text = "Масла";
+            checkBox8.Size = new Size(230, 30);
+            panel.Controls.Add(checkBox8);
+
+            CheckBox checkBox9 = new CheckBox();
+            checkBox9.Text = "Буталон";
+            checkBox9.Size = new Size(230, 30);
+            panel.Controls.Add(checkBox9);
+
+            CheckBox checkBox10 = new CheckBox();
+            checkBox10.Text = "Водяной пар";
+            checkBox10.Size = new Size(230, 30);
+            panel.Controls.Add(checkBox10);
+
+            CheckBox checkBox11 = new CheckBox();
+            checkBox11.Text = "Сухие нейтральные";
+            checkBox11.Size = new Size(230, 30);
+            panel.Controls.Add(checkBox11);
+
+            CheckBox checkBox20 = new CheckBox();
+            checkBox20.Text = "Инертные газы";
+            checkBox20.Size = new Size(230, 30);
+            panel.Controls.Add(checkBox20);
+
+            CheckBox checkBox12 = new CheckBox();
+            checkBox12.Text = "Легкие нефтепродукты";
+            checkBox12.Size = new Size(230, 30);
+            panel.Controls.Add(checkBox12);
+
+            CheckBox checkBox13 = new CheckBox();
+            checkBox13.Text = "Сниженные углеводороды";
+            checkBox13.Size = new Size(230, 30);
+            panel.Controls.Add(checkBox13);
+
+            CheckBox checkBox14 = new CheckBox();
+            checkBox14.Text = "Кислород";
+            checkBox14.Size = new Size(230, 30);
+            panel.Controls.Add(checkBox14);
+
+            CheckBox checkBox21 = new CheckBox();
+            checkBox21.Text = "Азот";
+            checkBox21.Size = new Size(230, 30);
+            panel.Controls.Add(checkBox21);
+
+            CheckBox checkBox15 = new CheckBox();
+            checkBox15.Text = "Коксовый газ";
+            checkBox15.Size = new Size(230, 30);
+            panel.Controls.Add(checkBox15);
+
+            CheckBox checkBox16 = new CheckBox();
+            checkBox16.Text = "Углеводороды жидкие";
+            checkBox16.Size = new Size(230, 30);
+            panel.Controls.Add(checkBox16);
+
+            CheckBox checkBox22 = new CheckBox();
+            checkBox22.Text = "Углеводороды газообразные";
+            checkBox22.Size = new Size(230, 30);
+            panel.Controls.Add(checkBox22);
+
+            CheckBox checkBox17 = new CheckBox();
+            checkBox17.Text = "Растворители";
+            checkBox17.Size = new Size(230, 30);
+            panel.Controls.Add(checkBox17);
+
+            CheckBox checkBox18 = new CheckBox();
+            checkBox18.Text = "Щелочи любой концентрации";
+            checkBox18.Size = new Size(230, 30);
+            panel.Controls.Add(checkBox18);
+
+            CheckBox checkBox23 = new CheckBox();
+            checkBox23.Text = "Кислоты любой концентрации";
+            checkBox23.Size = new Size(230, 30);
+            panel.Controls.Add(checkBox23);
+        }
+
+        private void PicBox_MouseEnter(object sender, EventArgs e)
+        {
+            if (sender is Control x)
+                x.BackColor = Color.Red;
+        }
+
+        private void PicBox_MouseLeave(object sender, EventArgs e)
+        {
+            if (sender is Control x)
+                x.BackColor = SystemColors.Control;
+        }
+
+        private void picBox_Paint(object sender, PaintEventArgs e)
+        {
+          
+            var count = 100;
+            var step = 0.1f;
+            var points = Enumerable.Range(0, count)
+                .Select(x => step * x - step * count / 2)
+                .Select(x => new PointF(x+f , k * (float)Math.Pow(x, n)+c));
+            var blackPen = new Pen(Color.Black, 1);
+            e.Graphics.TranslateTransform(picBoxPaint.Width / 2, picBoxPaint.Height / 2);
+            e.Graphics.ScaleTransform(1, -1);
+            e.Graphics.ScaleTransform(e.Graphics.DpiX / 2.54f, e.Graphics.DpiY / 2.54f);
+            var penTransform = e.Graphics.Transform.Clone();
+            penTransform.Invert();
+            blackPen.Transform = penTransform;
+            var grayPen = new Pen(Color.LightGray, 1);
+            grayPen.Transform = penTransform;
+
+            for (var x = -10; x <= 10; ++x)
+            {
+                var pen = x == 0 ? blackPen : grayPen;
+                e.Graphics.DrawLine(pen, x, -10, x, 10);
+                e.Graphics.DrawLine(pen, -10, x, 10, x);
+            }
+            e.Graphics.DrawLines(blackPen, points.ToArray());
+        }
+
+       
     }
 }
